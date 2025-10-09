9286bd4e905d427f2c356955d2ba2092a6cb444a
root <root@DESKTOP-8MLGNB0.>
2025-10-10 02:03:42 +0800

    v10.10.2 - 修复WSL环境Chrome无法启动的问题（添加single-process等参数）

--- server/douyin_scraper_v2.py
+++ server/douyin_scraper_v2.py
@@ -35,10 +35,17 @@ class DouyinScraperV2:
         
         chrome_options = Options()
         
+        # 无头模式配置（WSL环境必须）
         if self.headless:
             chrome_options.add_argument('--headless=new')
             chrome_options.add_argument('--no-sandbox')
             chrome_options.add_argument('--disable-dev-shm-usage')
+            chrome_options.add_argument('--disable-gpu')
+            chrome_options.add_argument('--disable-software-rasterizer')
+            chrome_options.add_argument('--disable-extensions')
+            chrome_options.add_argument('--disable-setuid-sandbox')
+            chrome_options.add_argument('--single-process')  # 重要：防止WSL中的多进程问题
+            chrome_options.add_argument('--remote-debugging-port=9222')
         
         # 反爬虫设置
         chrome_options.add_argument('--disable-blink-features=AutomationControlled')
